9e00225b418c916808bc4b91e5a3b1d11abb595a
jiyoungsophia <sophiathedev@gmail.com>
2025-08-27 17:31:34 +0900

    chore: #12 - font와 애니메이션 관련 extension 수정

--- FIG/FIG/Utils/UIFont+.swift
+++ FIG/FIG/Utils/UIFont+.swift
@@ -9,14 +9,14 @@ import UIKit
 
 extension UIFont {
     func withWeight(_ weight: UIFont.Weight) -> UIFont {
-      guard let textStyle = fontDescriptor.fontAttributes[UIFontDescriptor.AttributeName.textStyle] as? UIFont.TextStyle else {
-        return self
-      }
+        guard let textStyle = fontDescriptor.fontAttributes[UIFontDescriptor.AttributeName.textStyle] as? UIFont.TextStyle else {
+            return self
+        }
         let traitCollection = UITraitCollection(preferredContentSizeCategory: .large)
-      let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: textStyle, compatibleWith: traitCollection)
-      return UIFontMetrics(forTextStyle: textStyle).scaledFont(
-        for: .systemFont(ofSize: descriptor.pointSize, weight: weight),
-        compatibleWith: nil
-      )
+        let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: textStyle, compatibleWith: traitCollection)
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(
+            for: .systemFont(ofSize: descriptor.pointSize, weight: weight),
+            compatibleWith: nil
+        )
     }
 }
--- FIG/FIG/Utils/UISpringTimingParameters+.swift
+++ FIG/FIG/Utils/UISpringTimingParameters+.swift
@@ -8,29 +8,29 @@
 import UIKit
 
 extension UISpringTimingParameters {
-  @inlinable static func spring(duration: TimeInterval, bounce: CGFloat, initialVelocity: CGVector) -> UISpringTimingParameters {
-    if #available(iOS 17.0, *) {
-      return UISpringTimingParameters(duration: duration, bounce: bounce, initialVelocity: initialVelocity)
+    @inlinable static func spring(duration: TimeInterval, bounce: CGFloat, initialVelocity: CGVector) -> UISpringTimingParameters {
+        if #available(iOS 17.0, *) {
+            return UISpringTimingParameters(duration: duration, bounce: bounce, initialVelocity: initialVelocity)
+        }
+        let dampingRatio = if bounce >= 0 {
+            1 - bounce
+        } else {
+            1 / (1 + bounce)
+        }
+        return UISpringTimingParameters(dampingRatio: dampingRatio, initialVelocity: initialVelocity)
     }
-    let dampingRatio = if bounce >= 0 {
-      1 - bounce
-    } else {
-      1 / (1 + bounce)
-    }
-    return UISpringTimingParameters(dampingRatio: dampingRatio, initialVelocity: initialVelocity)
-  }
 }
 
 extension UITimingCurveProvider where Self == UISpringTimingParameters {
-  static func smooth(duration: CGFloat, initialVelocity: CGVector = .zero) -> UISpringTimingParameters {
-    UISpringTimingParameters.spring(duration: duration, bounce: 0.0, initialVelocity: initialVelocity)
-  }
-
-  static func snappy(duration: CGFloat, initialVelocity: CGVector = .zero) -> UISpringTimingParameters {
-    UISpringTimingParameters.spring(duration: duration, bounce: 0.15, initialVelocity: initialVelocity)
-  }
-
-  static func bouncy(duration: CGFloat, initialVelocity: CGVector = .zero) -> UISpringTimingParameters {
-    UISpringTimingParameters.spring(duration: duration, bounce: 0.3, initialVelocity: initialVelocity)
-  }
+    static func smooth(duration: CGFloat, initialVelocity: CGVector = .zero) -> UISpringTimingParameters {
+        UISpringTimingParameters.spring(duration: duration, bounce: 0.0, initialVelocity: initialVelocity)
+    }
+    
+    static func snappy(duration: CGFloat, initialVelocity: CGVector = .zero) -> UISpringTimingParameters {
+        UISpringTimingParameters.spring(duration: duration, bounce: 0.15, initialVelocity: initialVelocity)
+    }
+    
+    static func bouncy(duration: CGFloat, initialVelocity: CGVector = .zero) -> UISpringTimingParameters {
+        UISpringTimingParameters.spring(duration: duration, bounce: 0.3, initialVelocity: initialVelocity)
+    }
 }
