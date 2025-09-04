284964f46c12b811d5f1fad213ba64e7b54598bd
Pjh01 <estelle0102@naver.com>
2025-09-04 15:10:18 +0900

    chore: #33 - 차트 관련 뷰 수정

--- FIG/FIG/Scene/Chart/View/ChartCategoryHeaderView.swift
+++ FIG/FIG/Scene/Chart/View/ChartCategoryHeaderView.swift
@@ -12,26 +12,34 @@ import Then
 final class ChartCategoryHeaderView: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let stackView = UIStackView(axis: .horizontal, spacing: 10) {
-            UILabel().then {
-                $0.text = "카테고리별 금액"
-                $0.font = .preferredFont(forTextStyle: .body)
-                $0.textColor = .charcoal
-            }
-            UIStackView(axis: .vertical, alignment: .trailing) {
+        let stackView = UIStackView(axis: .vertical, spacing: 10) {
+            UIStackView(axis: .horizontal, spacing: 10) {
                 UILabel().then {
-                    $0.text = "총액"
-                    $0.font = .preferredFont(forTextStyle: .caption1)
+                    $0.text = "카테고리별 금액"
+                    $0.font = .preferredFont(forTextStyle: .body)
                     $0.textColor = .charcoal
-                    $0.setContentHuggingPriority(.required, for: .horizontal)
-                    $0.setContentCompressionResistancePriority(.required, for: .horizontal)
                 }
-                UILabel().then {
-                    $0.text = "지난달 대비"
-                    $0.font = .preferredFont(forTextStyle: .caption1)
-                    $0.textColor = .gray1
-                    $0.setContentHuggingPriority(.required, for: .horizontal)
-                    $0.setContentCompressionResistancePriority(.required, for: .horizontal)
+                UIStackView(axis: .vertical, alignment: .trailing) {
+                    UILabel().then {
+                        $0.text = "총액"
+                        $0.font = .preferredFont(forTextStyle: .caption1)
+                        $0.textColor = .charcoal
+                        $0.setContentHuggingPriority(.required, for: .horizontal)
+                        $0.setContentCompressionResistancePriority(.required, for: .horizontal)
+                    }
+                    UILabel().then {
+                        $0.text = "지난달 대비"
+                        $0.font = .preferredFont(forTextStyle: .caption1)
+                        $0.textColor = .gray1
+                        $0.setContentHuggingPriority(.required, for: .horizontal)
+                        $0.setContentCompressionResistancePriority(.required, for: .horizontal)
+                    }
+                }
+            }
+            UIView().then {
+                $0.backgroundColor = .gray3
+                $0.snp.makeConstraints {
+                    $0.height.equalTo(1)
                 }
             }
         }
@@ -39,7 +47,7 @@ final class ChartCategoryHeaderView: UICollectionReusableView {
         stackView.snp.makeConstraints {
             $0.top.equalToSuperview().inset(16)
             $0.leading.trailing.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(10)
+            $0.bottom.equalToSuperview()
         }
     }
     
