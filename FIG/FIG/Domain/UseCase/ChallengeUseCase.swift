abc37a50700d3dd7e0365a10e9d94c80f371f7ef
jiyoungsophia <sophiathedev@gmail.com>
2025-09-11 16:49:04 +0900

    refactor: #52 - challengeList에서 UseCase 사용하도록 리팩토링

--- FIG/FIG/Domain/UseCase/ChallengeUseCase.swift
+++ FIG/FIG/Domain/UseCase/ChallengeUseCase.swift
@@ -20,6 +20,18 @@ final class ChallengeUseCase {
         self.transactionRepository = transactionRepository
     }
     
+    func getAllChallengesWithStatus() -> Observable<[Challenge]> {
+        return challengeRepository.fetchAllChallenges()  // 전체 조회
+            .flatMap { [weak self] challenges -> Observable<[Challenge]> in
+                guard let self, !challenges.isEmpty else { return .just([]) }
+                return updateChallengesWithSpending(challenges)
+            }
+            .flatMap { [weak self] challenges -> Observable<[Challenge]> in
+                guard let self else { return .just([]) }
+                return updateStatus(challenges)
+            }
+    }
+    
     func getCurrentChallenges(year: Int, month: Int) -> Observable<[Challenge]> {
         return challengeRepository.fetchChallengesByMonth(year, month)
             .flatMap { [weak self] challenges -> Observable<[Challenge]> in
