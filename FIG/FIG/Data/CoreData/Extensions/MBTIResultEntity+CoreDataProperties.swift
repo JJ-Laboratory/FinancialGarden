59f915496d08b1eeaae09d998240e5c37f0ec8b1
Pjh01 <estelle0102@naver.com>
2025-09-17 16:57:39 +0900

    feat: #69 - MBTIResultEntity 수정

--- FIG/FIG/Data/CoreData/Extensions/MBTIResultEntity+CoreDataProperties.swift
+++ FIG/FIG/Data/CoreData/Extensions/MBTIResultEntity+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  MBTIResultEntity+CoreDataProperties.swift
 //  FIG
 //
-//  Created by estelle on 9/16/25.
+//  Created by estelle on 9/17/25.
 //
 //
 
@@ -16,10 +16,13 @@ extension MBTIResultEntity {
         return NSFetchRequest<MBTIResultEntity>(entityName: "MBTIResultEntity")
     }
 
-    @NSManaged public var mbti: String?
-    @NSManaged public var title: String?
     @NSManaged public var desc: String?
-    @NSManaged public var recommend: String?
     @NSManaged public var id: Int16
+    @NSManaged public var mbti: String?
+    @NSManaged public var duration: String?
+    @NSManaged public var title: String?
+    @NSManaged public var categoryID: UUID?
+    @NSManaged public var spendingLimit: Int32
+    @NSManaged public var reason: String?
 
 }
