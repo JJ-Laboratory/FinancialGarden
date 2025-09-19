5d63bcdde87c4bfaa669233747bc69ebef669130
Pjh01 <estelle0102@naver.com>
2025-09-19 15:44:19 +0900

    feat: #72 - gif 이미지 반영

--- FIG/FIG/Utils/Extensions/UIImage+.swift
+++ FIG/FIG/Utils/Extensions/UIImage+.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SwiftyGif
 
 extension UIImage {
     func resized(height: CGFloat) -> UIImage? {
@@ -16,6 +19,22 @@ extension UIImage {
         
         return renderer.image { context in
             self.draw(in: CGRect(origin: .zero, size: newSize))
+        }
+    }
+}
+
+extension Reactive where Base: UIImageView {
+    var gifImage: Binder<String> {
+        return Binder(self.base) { imageView, gifName in
+            do {
+                let gif = try UIImage(gifName: "\(gifName).gif")
+                imageView.setGifImage(gif, loopCount: -1)
+            } catch {
+                print("GIF 로드 실패: \(error)")
+                if let defaultImage = try? UIImage(gifName: "default.gif") {
+                    imageView.setGifImage(defaultImage, loopCount: -1)
+                }
+            }
         }
     }
 }
